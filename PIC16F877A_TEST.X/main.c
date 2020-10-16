47d9b3e4a5aa9124bf63af904fedfc9d2befe357
MOHAB\mohab <mohab.ahmed.2032@gmail.com>
2020-10-16 02:22:22 +0200

    Files for TEST project.

---
+++ PIC16F877A_TEST.X/main.c
@@ -0,0 +1,32 @@
+/*
+ * File:   main.c
+ * Author: mohab
+ *
+ * Created on October 14, 2020, 9:55 PM
+ */
+
+
+#include <xc.h>
+#include"config.h"
+#define _XTAL_FREQ 4000000      //set the crystal frequency
+
+void main(void) {
+    TRISBbits.TRISB0=1;         //Port B0 input
+    TRISBbits.TRISB1=0;         //Port B1 output
+    PORTBbits.RB1=0;            //output low LED off
+
+    while(1)
+    {
+        if(PORTBbits.RB0)       
+        {
+            PORTBbits.RB1=1;
+            __delay_ms(250);
+            PORTBbits.RB1=0;
+            __delay_ms(250);
+        }
+        else
+            PORTBbits.RB1=0;
+ 
+    }
+    return;
+}
